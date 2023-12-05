b61c9c6bd0dbe9f6558bcdea6c52c0ab31130b63
Hodor <geoffrey.aulombard@gmail.com>
2023-12-06 00:01:45 +0100

    tuto

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-
-
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [HeaderComponent]
+  declarations: [HeaderComponent],
+  imports: [CommonModule, SharedModule,RouterModule],
+  exports: [HeaderComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
