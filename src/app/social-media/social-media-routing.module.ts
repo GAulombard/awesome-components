f0443babf77e9ca98740d83e23f0bec311dfbf9d
Hodor <geoffrey.aulombard@gmail.com>
2023-12-06 23:26:31 +0100

    tuto

--- src/app/social-media/social-media-routing.module.ts
+++ src/app/social-media/social-media-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostResolver } from './resolvers/post.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostListComponent, resolve: { post: PostResolver } },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SocialMediaRoutingModule { }
+export class SocialMediaRoutingModule {}
--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SocialMediaRoutingModule } from './social-media-routing.module';
-
+import { PostService } from './services/post.service';
+import { PostResolver } from './resolvers/post.resolver';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SocialMediaRoutingModule
-  ]
+  declarations: [
+    PostListComponent
+  ],
+  imports: [CommonModule, SocialMediaRoutingModule],
+  providers: [PostService,PostResolver],
 })
-export class SocialMediaModule { }
+export class SocialMediaModule {}
