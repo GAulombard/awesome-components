0f8da6f44b462d7bfb56582742210e03ef871f95
Hodor <geoffrey.aulombard@gmail.com>
2023-12-07 23:28:23 +0100

    tuto

--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -6,13 +6,14 @@ import { PostService } from './services/post.service';
 import { PostResolver } from './resolvers/post.resolver';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     PostListComponent,
     PostListItemComponent
   ],
-  imports: [CommonModule, SocialMediaRoutingModule],
+  imports: [CommonModule, SocialMediaRoutingModule,SharedModule],
   providers: [PostService,PostResolver],
 })
 export class SocialMediaModule {}
