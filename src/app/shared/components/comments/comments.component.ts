40ad35c2dd9eafd05d3a6fd93b85b410fa7adb45
Hodor-Fixe <geoffrey.aulombard@gmail.com>
2023-12-16 17:37:41 +0100

    tuto

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -3,6 +3,11 @@ import { Comment } from '../../../core/models/comment.model';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import {
   animate,
+  animateChild,
+  group,
+  query,
+  sequence,
+  stagger,
   state,
   style,
   transition,
@@ -14,6 +19,11 @@ import {
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss',
   animations: [
+    trigger('list', [
+      transition(':enter', [
+        query('@listItem', [stagger(50, [animateChild()])]),
+      ]),
+    ]),
     trigger('listItem', [
       state(
         'default',
@@ -34,6 +44,11 @@ import {
       transition('default => active', [animate('100ms ease-in-out')]),
       transition('active => default', [animate('250ms ease-in-out')]),
       transition('void => *', [
+        query('.comment-text, .comment-date', [
+          style({
+            opacity: 0,
+          }),
+        ]),
         style({
           transform: 'translateX(-100%)',
           opacity: 0,
@@ -47,6 +62,24 @@ import {
             'background-color': 'white',
           })
         ),
+        group([
+          sequence([
+            animate(
+              '250ms',
+              style({
+                'background-color': 'rgb(201,157,242)',
+              })
+            ),
+            animate(
+              '250ms',
+              style({
+                'background-color': 'white',
+              })
+            ),
+          ]),
+          query('.comment-text', [animate('250ms', style({ opacity: 1 }))]),
+          query('.comment-date', [animate('500ms', style({ opacity: 1 }))]),
+        ]),
       ]),
     ]),
   ],
